6a477a43f0c755e45974a5c13db4e5c6b1789b5b
maurizio-papa <73225821+maurizio-papa@users.noreply.github.com>
2024-01-30 12:29:25 +0100

    Create download_all_epic.py

---
+++ download_all_epic.py
@@ -0,0 +1,11 @@
+import requests
+import shutil
+
+def download_file(url, file_name):
+
+    with requests.get(url, stream=True) as r:
+        with open(file_name, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
+if __main__ == '__name__':
+    download_file('https://data.bris.ac.uk/datasets/tar/2g1n6qdydwa9u22shpxqzp0t8m.zip')
