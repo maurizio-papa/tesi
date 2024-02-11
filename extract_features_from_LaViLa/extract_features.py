5a040efe79c816be9edac998f998547434ba0400
maurizio-papa <73225821+maurizio-papa@users.noreply.github.com>
2024-02-11 17:55:35 +0100

    Update extract_features.py

--- extract_features_from_LaViLa/extract_features.py
+++ extract_features_from_LaViLa/extract_features.py
@@ -18,7 +18,7 @@ import torchvision.transforms._transforms_video as transforms_video
 from sklearn.metrics import confusion_matrix
 import wandb
 
-from download_videos_and_convert_to_tensor.untar_img_and_convert_to_h5.load_images_from_hdf5
+from download_videos_and_convert_to_tensor.untar_img_and_convert_to_h5 import load_images_from_hdf5
 
 from lavila.data import datasets
 from lavila.data.video_transforms import Permute, SpatialCrop, TemporalCrop
@@ -49,21 +49,23 @@ transform = transforms.Compose([
     transforms_video.NormalizeVideo(mean=[108.3272985, 116.7460125, 104.09373615000001], std=[68.5005327, 66.6321579, 70.32316305])
     ])
 
- to_tensor = transforms.PILToTensor()
+to_tensor = transforms.PILToTensor()
 
 
-def load_model(ckpt_path = BASE_MODEL):
+def load_model(BASE_MODEL, FINETUNED_MODEL):
     """
     loads pre-trained and then fine-tuned model,
     removes the last layer and return the fine-tuned model
     """
+    ckpt = torch.load(BASE_MODEL, map_location='cpu')
+
     old_args = ckpt['args']
 
     state_dict = OrderedDict()
     for k, v in ckpt['state_dict'].items():
         state_dict[k.replace('module.', '')] = v
 
-    print(f'creating model: {old_args.model}")
+    print(f'creating model: {old_args.model}')
 
     model = getattr(models, old_args.model)(
         pretrained=old_args.load_visual_pretrained,
@@ -102,64 +104,30 @@ def load_model(ckpt_path = BASE_MODEL):
 
     model.fc_cls = nn.ModuleList([torch.nn.Identity(), torch.nn.Identity(), torch.nn.Identity()])
 
+
     return model
-
-
-def write_pickle(data, file_name):
-    with open(file_name, "wb") as f:
-        pickle.dump(data, f)
-
+    
 
 def main():     
     if not os.path.exists(FEATURE_DIR):
         os.makedirs(FEATURE_DIR)
 
-    model = load_model()
-
-    for participant in os.listdir(IMAGE_TENSOR_DIR):
-        for video in os.listdir(f'{IMAGE_TENSOR_DIR}/{participant}'):
-
-            if not os.path.exist(f'{FEATURE_DIR}/{participant}/{video}'):
-                os.makedirs(f'{FEATURE_DIR}/{participant}/{video}')
-        
-            file_name = os.getcwd() + f'/{FEATURE_DIR}/{participant}/{video}.h5'
-
-            with h5py.File(file_name, 'w') as file:
-                for i, clip_batch in enumerate(os.listdir(f'{IMAGE_TENSOR_DIR}/{participant}/{video}')):
-                    
-                    clip_batch = load_images_from_hdf5(clip_batch)
-                    clip_batch = ([to_tensor((clip_batch[t])) for t in clip_batch])
-                    clip_batch = torch.stack(clip_batch).float()
-                    clip_batch = transform(clip_batch).unsqueeze(0)
-                    feature = model(clip_batch)
-                    file.create_dataset(f'tensor_{i}', data= features)
-     
-def extract_features(file):
-    extracted_features = []
-    clip_batch = load_images_from_hdf5(file)
-    clip_batch = ([to_tensor((clip_batch[t])) for t in clip_batch])
-    length = len(clip_batch)
-    while (length - 16) > start:
-        start = 0
-        end = 16
-        stripe = 8 
-        current_batch = torch.stack(clip_batch[start : end]).float()
-        current_batch = transform(current_batch).unsqueeze(0)
-        start += stripe 
-        end += stripe 
-        feature = model(current_batch)[0]
-        extracted_features.append(feature)
-
-def main():     
-    if not os.path.exists(FEATURE_DIR):
-        os.makedirs(FEATURE_DIR)
-
-    model = load_model_without_head()
-
+    model = load_model(BASE_MODEL, FINETUNED_MODEL)
+    model.eval()
     for participant in os.listdir(TENSOR_DIR):
         for video in os.listdir(f'{TENSOR_DIR}/{participant}'):
-            with h5py.File(f'{video}.h5', 'w') as file:
-                for video in os.listdir(video):
-                    feature = extract_features(video)
-                    file.create_dataset(f"tensor_{i}", data = feature)
+            
+            if not os.path.exists(f'{FEATURE_DIR}/{participant}'):
+                os.makedirs(f'{FEATURE_DIR}/{participant}')
+                
+            with h5py.File(f'{FEATURE_DIR}/{participant}/{video}.h5', 'w') as file:
+                for i, clip_batch in enumerate(f'{TENSOR_DIR}/{participant}/{video}'):
+                    clip_batch = load_images_from_hdf5(f''{TENSOR_DIR}/{participant}/{video}/{clip_batch}')
+                    current_batch = [to_tensor(img) for img in clip_batch.values()]
+                    current_batch = torch.stack(current_batch).float()
+                    current_batch = transform(current_batch)
+                    current_batch = current_batch.unsqueeze(0)
+                    features = model(current_batch)[0]
+                    file.create_dataset(f'clip_{i}', data = features)
+                    del current_batch
 
