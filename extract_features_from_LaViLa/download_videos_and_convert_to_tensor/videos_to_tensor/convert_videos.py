7ed7831a53b95a90e5f7de41d8e823bdbc6c35dd
maurizio-papa <73225821+maurizio-papa@users.noreply.github.com>
2024-03-18 15:33:19 +0100

    Update convert_videos.py

--- extract_features_from_LaViLa/download_videos_and_convert_to_tensor/videos_to_tensor/convert_videos.py
+++ extract_features_from_LaViLa/download_videos_and_convert_to_tensor/videos_to_tensor/convert_videos.py
@@ -1,7 +1,7 @@
 from video_to_tensor import convert_videos_to_jpg, convert_jpg_to_tensor
 import time
 
-EPIC_KITCHENS_VIDEO_DIR = './videos'
+EPIC_KITCHENS_VIDEO_DIR = './videos_converted'
 EPIC_KITCHENS_IMAGE_DIR = './images'
 EPIC_KITCHENS_TENSOR_DIR = './tensor'
 
