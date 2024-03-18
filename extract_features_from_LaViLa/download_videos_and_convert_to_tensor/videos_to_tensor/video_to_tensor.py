b2da4cd432c869a724e791c60ab431620f711bcd
maurizio-papa <73225821+maurizio-papa@users.noreply.github.com>
2024-03-18 15:54:42 +0100

    Update video_to_tensor.py

--- extract_features_from_LaViLa/download_videos_and_convert_to_tensor/videos_to_tensor/video_to_tensor.py
+++ extract_features_from_LaViLa/download_videos_and_convert_to_tensor/videos_to_tensor/video_to_tensor.py
@@ -3,14 +3,29 @@ import subprocess
 import av
 import numpy as np
 import torch
-from download_videos_and_convert_to_tensor.videos_to_tensor.split_videos_in_jpg import split_video_to_jpg
-from image_to_tensor.image_to_tensor_h5 import images_to_hdf5
 import numpy as np
 import h5py
 from PIL import Image
 import io
 import os 
+import subprocess
 
+def split_video_to_jpg(input_video, output_directory, fps= 30):
+    '''
+    Split a video into JPEG frames and write them to disk.
+    Args:
+        input_video (str): Path to the input video file.
+        output_directory (str): Directory to save the extracted JPEG frames.
+        fps (int): Frames per second for extracting frames (default is 24).
+    '''
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    output_pattern = os.path.join(output_directory, 'frame_%04d.jpg')
+    command = f"ffmpeg -i {input_video} -vf fps={fps} {output_pattern}"
+
+    subprocess.run(command, shell=True)
+    
 def images_to_hdf5(input_directory, output_file):
     '''
     Convert images in input_directory to HDF5 format.
